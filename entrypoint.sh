f4b3cdd5e90cc252bfb202fe4920e19cf2a42d2d
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2021-01-30 16:13:02 +0100

    Entrypoint: fixed downloading of data.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 whoami
 date
 
-mkdir dataset
-wget https://www.kaggle.com/mlg-ulb/creditcardfraud/download -O ./dataset/credit_cards_transactions.csv
-
-
+kaggle datasets download -d mlg-ulb/creditcardfraud
+unzip creditcardfraud.zip
+rm creditcardfraud.zip
 
